e6fb1e4940e0f3d92a874f921c9fd5ca9191a532
Matheus Marinho <marinho2matheus@gmail.com>
2025-09-30 14:34:48 -0300

    item 2

--- App.java
+++ App.java
@@ -3,32 +3,54 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o primeiro número: ");
-        double n1 = scanner.nextDouble();
-        System.out.print("Digite o segundo número: ");
-        double n2 = scanner.nextDouble();
-        System.out.print("Digite o terceiro número: ");
-        double n3 = scanner.nextDouble();
-    
-        double maior = n1;
-        if (n2 > maior){
-            maior = n2;
-        } 
-        if (n3 > maior){
-            maior = n3;
-        }
-        double menor = n1;
-        if (n2 < menor){
-             menor = n2;
-        }
-        if (n3 < menor){
-            menor = n3;
-        }
 
-        double media = (n1 + n2 + n3) / 3.0;
-        System.out.println("Maior número: " + maior);
-        System.out.println("Menor número: " + menor);
-        System.out.println("Média aritmética: " + media);
+        System.out.print("Digite o valor da compra: R$ ");
+        int valorCompra = scanner.nextInt();
+
+        System.out.print("Digite o valor pago: R$ ");
+        int valorPago = scanner.nextInt();
+
+        if (valorPago < valorCompra) {
+            System.out.println("Quantia paga é insuficiente para realizar a compra!");
+        } else {
+            int troco = valorPago - valorCompra;
+            System.out.println("\nTroco a ser devolvido: R$ " + troco);
+
+            int notas50 = troco / 50;
+            troco = troco % 50;
+            if (notas50 > 0) {
+                System.out.println(notas50 + " nota(s) de R$ 50");
+            }
+
+            int notas20 = troco / 20;
+            troco = troco % 20;
+            if (notas20 > 0) {
+                System.out.println(notas20 + " nota(s) de R$ 20");
+            }
+
+            int notas10 = troco / 10;
+            troco = troco % 10;
+            if (notas10 > 0) {
+                System.out.println(notas10 + " nota(s) de R$ 10");
+            }
+
+            int notas5 = troco / 5;
+            troco = troco % 5;
+            if (notas5 > 0) {
+                System.out.println(notas5 + " nota(s) de R$ 5");
+            }
+
+            int notas2 = troco / 2;
+            troco = troco % 2;
+            if (notas2 > 0) {
+                System.out.println(notas2 + " nota(s) de R$ 2");
+            }
+
+            int notas1 = troco;
+            if (notas1 > 0) {
+                System.out.println(notas1 + " nota(s) de R$ 1");
+            }
+        }
 
         scanner.close();
     }
